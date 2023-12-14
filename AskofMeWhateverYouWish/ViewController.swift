60a731dc79a98e78c3afaf5bf1e5bfbd7b8b08bc
İsmail Can Akgün <ismailcanakg1@gmail.com>
2023-12-15 00:00:16 +0300

    Initial Commit

---
+++ AskofMeWhateverYouWish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AskofMeWhateverYouWish
+//
+//  Created by İsmail Can Akgün on 15.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
